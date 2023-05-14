592269b7617a36553638fcc2fa6a8473fbe09ef0
RearizthMD <astarohhellprince@gmail.com>
2023-05-14 21:35:14 +0700

    first commit

--- artikel/admin/artikel/delete.php
+++ artikel/admin/artikel/delete.php
@@ -1,4 +1,7 @@
 <?php
+
+
+
 // include config connection file
 include_once("../../config.php");
 include('session.php');
@@ -18,7 +21,7 @@ if ($result->num_rows ==  0) {
 }
 // Delete user row from table based on given id
 
-$result = mysqli_query($mysqli, "DELETE FROM tb_artikel WHERE id= $id");
+$result = mysqli_query($mysqli, "DELETE FROM tb_artikel WHERE id=$id");
 
 // After delete redirect to Home, so that latest user list will be displayed.
 header("Location:../dashboard.php?page=artikel");
